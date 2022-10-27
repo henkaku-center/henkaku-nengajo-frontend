06f3c2f8502b629395a6375c6d5e1eb995198280
yuwa2020 <yudaihirata411@gmail.com>
2022-10-27 21:49:29 +0900

    Add wagmi and allow user to connect wallet
    Mostly my code is copied from wagmi's website. As some file directory dose not correct, please tell me.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  chain,
+  defaultChains,
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
 
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [publicProvider()],
+)
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <WagmiConfig client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </WagmiConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import type { NextPage } from 'next'
 import { Box, Container, Heading } from '@chakra-ui/react'
+import { useAccount, useConnect, useEnsName } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
+function Profile() {
+  const { address, isConnected } = useAccount()
+  const { data: ensName } = useEnsName({ address })
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+
+  if (isConnected) return <div>Connected to {ensName ?? address}</div>
+  return <button onClick={() => connect()}>Connect Wallet</button>
+}
 const Home: NextPage = () => {
   return (
     <Box>
@@ -8,9 +20,12 @@ const Home: NextPage = () => {
         <Heading as="h1" size="4xl">
           Henkaku Nengajo
         </Heading>
+        <Profile />
       </Container>
     </Box>
   )
 }
+
+
 
 export default Home
