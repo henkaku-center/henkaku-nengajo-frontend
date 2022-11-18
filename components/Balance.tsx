aa9a2dec1c6ddbda56fa7b2669c13a39f3f9d875
dueyama <98719466+dueyama@users.noreply.github.com>
2022-11-18 10:53:27 +0900

    Update Balance.tsx
    add t or f check for useBalance to check on Polygon or Not.

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -5,7 +5,7 @@ export function Balance() {
   const henkakuV2 = '0x0cc91a5FFC2E9370eC565Ab42ECE33bbC08C11a2'
   const { address, isConnected } = useAccount()
 
-  const { data } = useBalance({
+  const { data, isSuccess } = useBalance({
     address: address,
     token: henkakuV2,
     onError(error) {
@@ -17,6 +17,7 @@ export function Balance() {
   })
 
   if (!isConnected) return <></>
+  if (!isSuccess) return <>Not on Polygon?</>
   return (
     <Box>
       You have {data?.formatted} {data?.symbol} now.
