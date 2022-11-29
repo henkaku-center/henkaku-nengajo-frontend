e96d90657d1f0e384c08906bbab059a0ca5746ac
dueyama <98719466+dueyama@users.noreply.github.com>
2022-11-29 11:57:07 +0900

    Add approve functions

---
+++ hooks/useApproval.ts
@@ -0,0 +1,91 @@
+import { BigNumber, ethers } from 'ethers'
+import { useState } from 'react'
+import {
+  erc20ABI,
+  useContractRead,
+  useContractWrite,
+  useContractEvent,
+  usePrepareContractWrite
+} from 'wagmi'
+
+const APPROVE_CALLBACK_STATUS = {
+  PENDING: 1,
+  FAIL: 2,
+  FINISH: 3
+}
+
+const useApprove = (erc20: string, spender: string) => {
+  console.log(spender)
+  const [status, setStatus] = useState<number>()
+  const { config } = usePrepareContractWrite({
+    address: erc20,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: [spender as `0x${string}`, ethers.constants.MaxUint256]
+  })
+
+  const { write: contractApprove } = useContractWrite({
+    ...config,
+    onError(error) {
+      console.log('approve ERROR:', error)
+      setStatus(APPROVE_CALLBACK_STATUS.FAIL)
+    },
+    onSuccess(data) {
+      console.log('approve SUCCESS:', data)
+      setStatus(APPROVE_CALLBACK_STATUS.PENDING)
+    }
+  })
+
+  return {
+    status: status,
+    approve: () => contractApprove?.()
+  }
+}
+
+const useApproval = (
+  erc20: string,
+  spenderAddress: string,
+  address: string | undefined
+) => {
+  const [approved, setApprove] = useState<boolean>()
+  const [allowanceValue, setAllowanceValue] = useState<BigNumber>()
+
+  useContractRead({
+    address: erc20,
+    abi: erc20ABI,
+    functionName: 'allowance',
+    args: [
+      (address as `0x${string}`) || ethers.constants.AddressZero,
+      spenderAddress as `0x${string}`
+    ],
+    onSuccess(data) {
+      setApprove(data?.gt(1000) ? true : false)
+    }
+  })
+
+  try {
+    useContractEvent({
+      address: erc20,
+      abi: erc20ABI,
+      eventName: 'Approval',
+      listener(owner, eventAddress, value) {
+        if (owner == address && eventAddress == spenderAddress) {
+          setApprove(value?.gt(1000) ? true : false)
+          setAllowanceValue(value)
+        }
+      }
+    })
+  } catch (e: any) {
+    console.error(e) // with different chain it occurs
+    if (e?.code != ethers.errors.INVALID_ARGUMENT) {
+      throw e
+    }
+  }
+
+  return {
+    approved,
+    allowanceValue
+  }
+}
+
+export { useApproval, useApprove, APPROVE_CALLBACK_STATUS }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from 'next'
 import { Box, Container, Heading } from '@chakra-ui/react'
 import { useAccount, useConnect, useEnsName } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
-import { useMounted } from '@/hooks'
+import { Approve } from 'components/metaMask/Approve'
+import { useMounted, useApproval } from '@/hooks'
 import Layout from '@/components/Layout'
 import { Connect } from '@/components/Connect'
 import { Profile } from '@/components/Profile'
@@ -12,6 +13,14 @@ import useTranslation from 'next-translate/useTranslation'
 const Home: NextPage = () => {
   const isMounted = useMounted()
   const { t } = useTranslation('common')
+  const { address } = useAccount()
+  const henkakuV2 = process.env
+    .NEXT_PUBLIC_CONTRACT_HENKAKUV2_ADDRESS as `0x${string}`
+  const nengajo = process.env
+    .NEXT_PUBLIC_CONTRACT_NENGAJO_ADDRESS as `0x${string}`
+  const { approved, allowanceValue } = useApproval(henkakuV2, nengajo, address)
+  // const approved = useApproval(henkakuV2, nengajo, address)
+
   return (
     <Layout>
       <Heading as="h1" size="4xl">
@@ -30,6 +39,20 @@ const Home: NextPage = () => {
       {isMounted && (
         <Box mt="2em">
           <Balance />
+        </Box>
+      )}
+      {approved ? (
+        <Box mt="2em">
+          <p>
+            Your allowance to {nengajo.toString()} is{' '}
+            {allowanceValue?.toString()} HENKAKU.
+          </p>
+        </Box>
+      ) : (
+        <Box mt="2em">
+          <Approve erc20={henkakuV2} spender={nengajo} style={{ width: '90%' }}>
+            {'approve'}
+          </Approve>
         </Box>
       )}
     </Layout>
