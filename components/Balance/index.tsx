b1f48ef362da5e3936e713e0050c039a62507dfe
sushi-yam <sushi.yam.jp@gmail.com>
2022-12-06 01:37:27 -0700

    setup for localizing balance component

--- components/Balance/index.tsx
+++ components/Balance/index.tsx
@@ -2,9 +2,11 @@ import { Box } from '@chakra-ui/react'
 import { useAccount, useBalance } from 'wagmi'
 import { getContractAddress } from '@/utils/contractAddresses'
 import { useChainId } from '@/hooks'
+import useTranslation from 'next-translate/useTranslation'
 
 export function Balance() {
   const { chainId, wrongNetwork } = useChainId()
+  const { t } = useTranslation('balance')
   const henkakuV2 = getContractAddress({
     name: 'henkakuErc20',
     chainId: chainId
@@ -29,7 +31,7 @@ export function Balance() {
   })
 
   if (!isConnected) return <></>
-  if (wrongNetwork) return <>Not on expected chain</>
+  if (wrongNetwork) return <>{t('NOT_EXPECTED_CHAIN')}</>
   if (!isSuccess) return <>Balance check failed</>
   return (
     <Box>
