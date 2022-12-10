74c3b2d09176bc5a8d12ed7e9b40974e4c0e086f
Alejandro Cremades <ale@alecrem.com>
2022-12-10 18:14:28 +0900

    Taking the nengajo title from a text input field

--- pages/create.tsx
+++ pages/create.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
   const isMounted = useMounted()
   const { t } = useTranslation('common')
   const { isConnected, address } = useAccount()
-  const [metadataName, setMetadataName] = useState('Test') // TODO: replace with form input
+  const [metadataName, setMetadataName] = useState('')
   const [fileImg, setFileImg] = useState<File | null>()
   const [isLoading, setIsLoading] = useState(false)
   const [imageUri, setImageUri] = useState('')
@@ -34,8 +34,13 @@ const Home: NextPage = () => {
   const handleImageChange = async (e: any) => {
     setFileImg(e?.target?.files[0])
   }
+  const handleNameChange = async (e: any) => {
+    console.log(e.target.value)
+    setMetadataName(e.target.value)
+  }
   const sendFileToIPFS = async (e: any) => {
     if (fileImg === undefined || fileImg === null) return
+    if (metadataName === '') return
     setIsLoading(true)
     try {
       const formData = new FormData()
@@ -78,6 +83,7 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     if (imageUri === '') return
+    if (metadataUri !== '') return
     if (address === undefined) return
     if (metadataName === '') return
 
@@ -119,7 +125,7 @@ const Home: NextPage = () => {
     }
 
     putMetadataOnIPFS(metadata)
-  }, [imageUri, address, metadataName])
+  }, [imageUri, address, metadataName, metadataUri])
 
   return (
     <Layout>
@@ -138,8 +144,22 @@ const Home: NextPage = () => {
           </Heading>
           <FormControl color="white.700">
             <FormControl isRequired mt={5}>
-              <FormLabel htmlFor="imageFile">{t('PICTURE')}</FormLabel>
               <form onSubmit={sendFileToIPFS}>
+                <FormLabel mt="1em" htmlFor="nengajoName">
+                  Nengajo title
+                </FormLabel>
+                <Input
+                  variant="outline"
+                  id="nengajoName"
+                  type="text"
+                  isRequired={true}
+                  placeholder="Nengajo name"
+                  name="nengajoName"
+                  onChange={handleNameChange}
+                />
+                <FormLabel mt="1em" htmlFor="imageFile">
+                  {t('PICTURE')}
+                </FormLabel>
                 <Input
                   variant="outline"
                   id="imageFile"
@@ -151,13 +171,14 @@ const Home: NextPage = () => {
                   onChange={handleImageChange}
                 />
                 {metadataUri !== '' ? (
-                  <Box>
-                    {t('UPLOAD_SUCCESS')}
+                  <Box mt="1em">
+                    {t('UPLOAD_SUCCESS')} (
+                    <Link href={metadataUri}>Metadata JSON</Link>)
                     <Image
+                      mt="1em"
                       src={imageUri ?? ''}
                       alt={'Uploaded image: ' + imageUri}
                     ></Image>
-                    <Link href={metadataUri}>Metadata JSON</Link>
                   </Box>
                 ) : (
                   <Button
