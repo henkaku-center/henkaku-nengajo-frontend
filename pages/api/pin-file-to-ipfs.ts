c24b81a1a5acaefb2a43f104f6c557965de8a55f
Alejandro Cremades <ale@alecrem.com>
2022-12-21 00:26:26 +0900

    Cleaner form for API placeholders

--- pages/api/pin-file-to-ipfs.ts
+++ pages/api/pin-file-to-ipfs.ts
@@ -1,25 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+type PinResponseData = {
   status: string
   data?: {
     IpfsHash?: string
   }
   error?: string
 }
-export default function handler(
+const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method === 'POST') {
-    res.status(200).json({
-      status: 'success',
-      data: { IpfsHash: 'Qmb9iRztDn3uTu7VcpXo9tNXatYAJahGz3AWSkL2q7iYne' }
-      // TODO: actually pin to IPFS instead returning a placeholder hash
-    })
-  } else {
+  res: NextApiResponse<PinResponseData>
+) => {
+  if (req.method !== 'POST') {
     res
       .status(405)
       .json({ status: 'error', error: `'${req.method}' method not allowed` })
   }
+
+  let status = 200
+  let resultBody: PinResponseData = {
+    status: 'success',
+    data: { IpfsHash: 'Qmb9iRztDn3uTu7VcpXo9tNXatYAJahGz3AWSkL2q7iYne' }
+    // TODO: actually pin to IPFS instead returning a placeholder hash
+  }
+  })
+  res.status(status).json(resultBody)
 }
+
+export default handler
