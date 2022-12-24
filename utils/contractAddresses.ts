51a74b359842d77d514ae1b948f3d0b2d74f3d56
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-12-24 09:17:24 +0900

    add gas limit and polygon chain

--- utils/contractAddresses.ts
+++ utils/contractAddresses.ts
@@ -25,7 +25,7 @@ const contractAddresses: ContractAddresses = {
       (process.env.NEXT_PUBLIC_CONTRACT_NENGAJO_ADDRESS as `0x${string}`) ?? '',
     [chainId.polygonMumbai]: '0x0B829f0fb8E0E37FF16247c2992fa2625E834aDf',
     [chainId.goerli]: '0x6beD9e854eC468373B70a00d864E660b9F224D32',
-    [chainId.polygon]: '0x2C3530B4642ff8fCEb6ab5Fc740381a358968aF1'
+    [chainId.polygon]: '0x51410f51EB90846b08159ec24d2E164b077aD00e'
   }
 }
 
