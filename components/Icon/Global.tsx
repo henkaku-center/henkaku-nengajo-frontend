cf77d3658b6bfb4de4caa5d08a6649f059fe62be
imaichiyyy <imaichi17@gmail.com>
2022-12-26 21:58:13 +0900

    Fix the header for responsive layout

---
+++ components/Icon/Global.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from '@chakra-ui/icons'
+
+// using `path`
+const GlobalIcon = createIcon({
+  displayName: 'GlobalIcon',
+  viewBox: '0 0 24 24',
+  // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
+  path: (
+    <path
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+    ></path>
+  )
+})
+
+export default GlobalIcon
