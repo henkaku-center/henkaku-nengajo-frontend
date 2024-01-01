9d9139f3dd1c1df3409d85a007b29da07163814b
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-01-01 14:56:21 +0900

    ipfs env

--- utils/ipfs2http.ts
+++ utils/ipfs2http.ts
@@ -2,7 +2,6 @@ export const parseIpfs2Pinata = (ipfsURI?: string) => {
   if (!ipfsURI) return ''
   const rootCid = ipfsURI.split('ipfs://')[1]
   return `${
-    process.env.NEXT_PUBLIC_IPFS_GATEWAY_BASEURL ||
-    'https://gateway.pinata.cloud'
+    process.env.NEXT_PUBLIC_PINATA_GATEWAY || 'https://gateway.pinata.cloud'
   }/ipfs/${rootCid}`
 }
